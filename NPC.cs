d13abd6dd2c82d78276894ec16114ddaff1c092c
Zavarushhka <111383760+Zavarushhka@users.noreply.github.com>
2023-07-07 18:12:44 +0300

    Add files via upload

--- NPC.cs
+++ NPC.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    //Здоровье NPC
+    public int health;
+
+    //Уровень NPC
+    public int level;
+
+    //Скорость NPC
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        int health=100;
-        int level=2;
-        health = health + level;
-        print("health"+health);
+        health = 4;
+        level = 2;
+        health+= level;
+        print("здоровье игрока-" + health);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+       Vector3 newposition = transform.position;
+        newposition.z -= speed*Time.deltaTime;
+        transform.position = newposition;
 }
+       
+    }
